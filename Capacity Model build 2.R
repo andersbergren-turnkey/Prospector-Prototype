e915e6980abf6e095421cf17e7c46e623571514e
andersbergren-turnkey <andersbergren@turnkeyse.com>
2017-08-15 09:37:03 -0400

    Integrated staged tuning with model build

--- Capacity Model build 2.R
+++ Capacity Model build 2.R
@@ -165,56 +165,83 @@ xgb.meanimpute.lrn <- makeImputeWrapper(
 
 CV.tune_setting = makeResampleDesc("CV", iters = 4)
 
-### xgboost tuning ### (long run time)
+### Tuning stage 1 ###
 
-# Try a seperate nrounds tuning prior to tuning other params
+ctrl.grid1 <- makeTuneControlGrid(resolution = 10)
 
-#  Set parameter search space
-params <- makeParamSet(makeIntegerParam("max_depth",lower = 5L,upper = 10L),
-                       makeIntegerParam("min_child_weight",lower = 1L,upper = 20L),
-                       makeNumericParam("subsample",lower = 0.6,upper = 1),
-                       makeNumericParam("eta",lower = 0.01,upper = 0.2),
-                       makeNumericParam("colsample_bytree",lower = 0.5,upper = 1),
-                       makeIntegerParam("nrounds",lower = 100L,upper = 300L),
-                       makeNumericParam("gamma",lower = 0,upper = 0.2))
+params1 <- makeParamSet(makeIntegerParam("nrounds", lower = 50L, upper = 150L),
+                        makeDiscreteParam("eta", values = .1))
 
-# try a seperate gamma tuning after params are established
-
-# establish an appropriate number of instances and times to run
-ctrl.rand <- makeTuneControlRandom(maxit = 3)
-ctrl.irace <- makeTuneControlIrace(maxExperiments = 200L)
-
-# Run tuning
 parallelStartSocket(cpus = detectCores())
 
-xgb.tune <- tuneParams(learner = xgb.meanimpute.lrn,
-                       task = O.log.task,
-                       resampling = CV.tune_setting,
-                       measures = rmse,
-                       par.set = params,
-                       control = ctrl.rand,
-                       show.info = TRUE)
+xgb.params1 <- tuneParams(learner = xgb.meanimpute.lrn,
+                          task = O.log.task,
+                          resampling = CV.tune_setting,
+                          measures = rmse,
+                          par.set = params1,
+                          control = ctrl.grid1,
+                          show.info = TRUE)
 
 parallelStop()
 
+xgb.params1.lrn <- setHyperPars(xgb.meanimpute.lrn, par.vals = xgb.params1$x)
+
+### Tuning stage 2 ###
+
+ctrl.irace2 <- makeTuneControlIrace(maxExperiments = 500L)
+
+params2 <- makeParamSet(makeIntegerParam("max_depth",lower = 3L,upper = 10L),
+                        makeIntegerParam("min_child_weight",lower = 1L,upper = 20L),
+                        makeNumericParam("subsample",lower = 0.5,upper = 1),
+                        makeNumericParam("colsample_bytree",lower = 0.5,upper = 1),
+                        makeNumericParam("gamma",lower = 0,upper = 0.4))
+
+parallelStartSocket(cpus = detectCores())
+
+xgb.params2 <- tuneParams(learner = xgb.params1.lrn,
+                          task = O.log.task,
+                          resampling = CV.tune_setting,
+                          measures = rmse,
+                          par.set = params2,
+                          control = ctrl.irace2,
+                          show.info = TRUE)
+
+parallelStop()
+
+xgb.params2.lrn <- setHyperPars(xgb.params1.lrn, par.vals = xgb.params2$x)
+
 save_tuning <- TRUE
 if (save_tuning) {
-  saveRDS(xgb.tune, file = "xgb.tune.rds")
+  saveRDS(xgb.params2.lrn, file = "xgb.params2.lrn.rds")
 }
 
-import_tuning <- FALSE
-if (import_tuning) {
-  xgb.tune <- readRDS(file = "xgb.tune.rds")
-}
+### Tuning stage 3 ###
 
-xgb.tuned.lrn <- setHyperPars(xgb.impute.lrn, par.vals = xgb.tune$x)
+ctrl.rand3 <- makeTuneControlIrace(maxExperiments = 100L)
+
+params3 <- makeParamSet(makeIntegerParam("nrounds",lower = 100L,upper = 1000L),
+                        makeNumericParam("eta",lower = 0.01,upper = .1))
+
+parallelStartSocket(cpus = detectCores())
+
+xgb.params3 <- tuneParams(learner = xgb.params2.lrn,
+                          task = O.log.task,
+                          resampling = CV.tune_setting,
+                          measures = rmse,
+                          par.set = params3,
+                          control = ctrl.irace3,
+                          show.info = TRUE)
+
+parallelStop()
+
+xgb.params3.lrn <- setHyperPars(xgb.params2.lrn, par.vals = xgb.params3$x)
 
 # Train model -------------------------------------------------------------
 
 ### Run training ###
 parallelStartSocket(cpus = detectCores())
 
-trained_model = train(xgb.tuned.lrn, O.log.task)
+trained_model = train(xgb.params3.lrn, O.log.task)
 
 parallelStop()
 
@@ -334,7 +361,7 @@ SCORING %<>%
 
 
 
-# Predict -----------------------------------------------------------------
+# Predict and bin -----------------------------------------------------------------
 
 PREDICTION <- SCORING %>%
   bind_cols(predict(trained_model, newdata = SCORING)$data)
--- Staged Tuning testing.R
+++ Staged Tuning testing.R
@@ -1,12 +1,8 @@
-# set eta to .1 and tune nrounds at 100, 125, 150, 175, 200, 225, 250, 175, 300
-# use irace tuning on other 5 params
-# decrease eta to .05 and increase nrounds by x10
+CV.tune_setting = makeResampleDesc("CV", iters = 4)
 
-ctrl.rand <- makeTuneControlRandom(maxit = 15)
-ctrl.irace <- makeTuneControlIrace(maxExperiments = 500L)
-ctrl.grid <- makeTuneControlGrid(resolution = 3L)
+### Tuning stage 1 ###
 
-# Tuning stage 1
+ctrl.grid1 <- makeTuneControlGrid()
 
 params1 <- makeParamSet(makeDiscreteParam("nrounds", values = c(50, 75, 100, 125, 150)),
                         makeDiscreteParam("eta", values = .1))
@@ -18,16 +14,18 @@ xgb.params1 <- tuneParams(learner = xgb.meanimpute.lrn,
                           resampling = CV.tune_setting,
                           measures = rmse,
                           par.set = params1,
-                          control = ctrl.grid,
+                          control = ctrl.grid1,
                           show.info = TRUE)
 
 parallelStop()
 
 xgb.params1.lrn <- setHyperPars(xgb.meanimpute.lrn, par.vals = xgb.params1$x)
 
-# Tuning stage 2
+### Tuning stage 2 ###
 
-params2 <- makeParamSet(makeIntegerParam("max_depth",lower = 5L,upper = 10L),
+ctrl.irace2 <- makeTuneControlIrace(maxExperiments = 500L)
+
+params2 <- makeParamSet(makeIntegerParam("max_depth",lower = 4L,upper = 10L),
                         makeIntegerParam("min_child_weight",lower = 1L,upper = 20L),
                         makeNumericParam("subsample",lower = 0.5,upper = 1),
                         makeNumericParam("colsample_bytree",lower = 0.5,upper = 1),
@@ -40,17 +38,83 @@ xgb.params2 <- tuneParams(learner = xgb.params1.lrn,
                        resampling = CV.tune_setting,
                        measures = rmse,
                        par.set = params2,
-                       control = ctrl.irace,
+                       control = ctrl.irace2,
                        show.info = TRUE)
 
 parallelStop()
 
-xgb.param2.lrn <- setHyperPars(xgb.params1.lrn, par.vals = xgb.params2$x)
+xgb.params2.lrn <- setHyperPars(xgb.params1.lrn, par.vals = xgb.params2$x)
 
 save_tuning <- TRUE
 if (save_tuning) {
-  saveRDS(xgb.param2.lrn, file = "xgb.param2.lrn.rds")
+  saveRDS(xgb.params2.lrn, file = "xgb.params2.lrn.rds")
 }
 
-# Tuning stage 3
+### Tuning stage 3 ###
 
+ctrl.rand3 <- makeTuneRa(maxExperiments = 100L)
+
+params3 <- makeParamSet(makeIntegerParam("nrounds",lower = 100L,upper = 1000L),
+                        makeNumericParam("eta",lower = 0.01,upper = .1))
+
+parallelStartSocket(cpus = detectCores())
+
+xgb.params3 <- tuneParams(learner = xgb.params2.lrn,
+                          task = O.log.task,
+                          resampling = CV.tune_setting,
+                          measures = rmse,
+                          par.set = params3,
+                          control = ctrl.irace3,
+                          show.info = TRUE)
+
+parallelStop()
+
+xgb.params3.lrn <- setHyperPars(xgb.params2.lrn, par.vals = xgb.params3$x)
+
+### Old method ###
+
+# CV.tune_setting = makeResampleDesc("CV", iters = 4)
+# 
+# ### xgboost tuning ### (long run time)
+# 
+# # Try a seperate nrounds tuning prior to tuning other params
+# 
+# #  Set parameter search space
+# params <- makeParamSet(makeIntegerParam("max_depth",lower = 4L,upper = 10L),
+#                        makeIntegerParam("min_child_weight",lower = 1L,upper = 20L),
+#                        makeNumericParam("subsample",lower = 0.5,upper = 1),
+#                        makeNumericParam("eta",lower = 0.01,upper = 0.2),
+#                        makeNumericParam("colsample_bytree",lower = 0.5,upper = 1),
+#                        makeIntegerParam("nrounds",lower = 100L,upper = 300L),
+#                        makeNumericParam("gamma",lower = 0,upper = 0.2))
+# 
+# # try a seperate gamma tuning after params are established
+# 
+# # establish an appropriate number of instances and times to run
+# ctrl.rand <- makeTuneControlRandom(maxit = 3)
+# ctrl.irace <- makeTuneControlIrace(maxExperiments = 200L)
+# 
+# # Run tuning
+# parallelStartSocket(cpus = detectCores())
+# 
+# xgb.tune <- tuneParams(learner = xgb.meanimpute.lrn,
+#                        task = O.log.task,
+#                        resampling = CV.tune_setting,
+#                        measures = rmse,
+#                        par.set = params,
+#                        control = ctrl.rand,
+#                        show.info = TRUE)
+# 
+# parallelStop()
+# 
+# save_tuning <- TRUE
+# if (save_tuning) {
+#   saveRDS(xgb.tune, file = "xgb.tune.rds")
+# }
+# 
+# import_tuning <- FALSE
+# if (import_tuning) {
+#   xgb.tune <- readRDS(file = "xgb.tune.rds")
+# }
+# 
+# xgb.tuned.lrn <- setHyperPars(xgb.impute.lrn, par.vals = xgb.tune$x)
